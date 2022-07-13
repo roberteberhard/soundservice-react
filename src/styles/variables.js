51e6fcf22084839fa234a23406adc1520ed0f18f
Robert <robert@roberteberhard.com>
2022-07-13 22:40:56 +0100

    Modified global styles

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -11,15 +11,10 @@ const variables = css`
     --jetblack: #1e1e1e;
     --lightblack: #282828;
     --lightgrey: #999999;
-
-    --darkgrey: #3c3c3e;
-    --oilgrey: #4a4a4a;
-
+    --darkgrey: #242428;
     --primary: #97d058;
     --secondary: #64a10e;
-
     --text-color: #4a4a4a;
-    --server-error: #996667;
 
     --font-sans: 'MuseoRounded', 'Inter', 'San Francisco', 'SF Pro Text', -apple-system, system-ui, sans-serif;
     --font-mono: Courier, 'Courier New', 'Lucida Sans Typewriter', 'Lucida Typewriter', monospace;
@@ -39,16 +34,8 @@ const variables = css`
     --pad-md: 40px;
     --pad-sm: 25px;
 
-    --border-radius: 4px;
-
     --easing: cubic-bezier(0.645, 0.045, 0.355, 1);
     --transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
-
-    --hamburger-width: 30px;
-    --ham-before: top 0.1s ease-in 0.25s, opacity 0.1s ease-in;
-    --ham-before-active: top 0.1s ease-out, opacity 0.1s ease-out 0.12s;
-    --ham-after: bottom 0.1s ease-in 0.25s, transform 0.22s cubic-bezier(0.55, 0.055, 0.675, 0.19);
-    --ham-after-active: bottom 0.1s ease-out, transform 0.22s cubic-bezier(0.215, 0.61, 0.355, 1) 0.12s;
   }
 `
 
